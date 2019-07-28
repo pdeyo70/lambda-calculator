a47431a968abd66bf8fb81f81c583a278ff03d9f
Paul DeYonghe <p.deyonghe@gmail.com>
2019-07-28 11:56:13 -0500

    got a different batch of errors after replacing [object] with [object.value] in my const declaration for the state handlers. Bookmarking.

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -8,7 +8,7 @@ import operators from "../../../data.js";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [operatorState] = useState(operators);
+  const [operatorState] = useState(operators.operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -8,7 +8,7 @@ import specials from "../../../data.js";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-  const [specialState] = useState(specials);
+  const [specialState] = useState(specials.specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
