768816b50a34052a6715d3c0e9ee80ca9dfc53c4
Paul DeYonghe <p.deyonghe@gmail.com>
2019-07-30 20:26:02 -0500

    some elements rendering, some style added

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -8,7 +8,7 @@ import numbers from "../../../data";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
-  const [numberState] = useState(numbers.numbers);
+  const [numberState, setNumberState] = useState(numbers.numbers);
   console.log("numberState from Numbers: ", numberState);
   return (
     <div>
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -8,13 +8,13 @@ import operators from "../../../data.js";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [operatorState] = useState(operators.operators);
+  const [operatorState, setOperatorState] = useState(operators.operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
         component matching the name on the provided file. Pass
       it any props needed by the child component*/
-        operatorState.map((operator, index) => <OperatorButton key = { index } operator = { operator } />)
+        operatorState.map((operator, index) => <OperatorButton key = { operator.char } operator = { operator } />)
       }
     </div>
   );
